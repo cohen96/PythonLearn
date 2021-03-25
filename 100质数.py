db78ad67085cbde2c6e96c7259013e05a09f8f7b
cohen96 <cohen96@users.noreply.github.com>
2021-03-25 13:35:21 -0400

    12

--- 100质数.py
+++ 100质数.py
@@ -1,13 +1,13 @@
 # 100内的质数
+
 i = 2
 while i <= 100 :
-    flag = True
+    flag = True #记录i的状态是否为质 默认是
     j = 2
     while j < 0.5 * i :
         if i % j ==0 :
-            flag = False
+            flag = False #余0=非质
         j += 1
     if flag == True :
         print(i)
     i += 1
-
