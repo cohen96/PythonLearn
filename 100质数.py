ce866326d6ed2965a595c9d12c8543acb53ee8ad
cohen96 <cohen96@users.noreply.github.com>
2021-03-25 13:51:23 -0400

    1

--- 100质数.py
+++ 100质数.py
@@ -1,13 +1,17 @@
-# 100内的质数
-
+from time import *
+begin = time()
 i = 2
-while i <= 100 :
+while i <= 50000 :
     flag = True #记录i的状态是否为质 默认是
     j = 2
-    while j < 0.5 * i :
+    while j <= i ** 0.5 :
         if i % j ==0 :
             flag = False #余0=非质
+            break
         j += 1
     if flag == True :
-        print(i)
+        pass
+        #print(i)
     i += 1
+end = time()
+print('time?', end - begin , "s")
