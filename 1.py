c0114399eb52babddc9328f4bfe90ccc7b03ba41
cohen96 <cohen96@users.noreply.github.com>
2021-03-25 00:36:09 -0400

    1

--- 1.py
+++ 1.py
@@ -1,3 +1,14 @@
 i = 2
-while i <= 100
+while i <= 100 :
+    flag = True
+    j = 2
+    while j < 0.5 * i :
+        if i % j ==0 :
+            flag = False
+        j += 1
+    if flag == True :
+        print(i)
+
+
+    i += 1
 
